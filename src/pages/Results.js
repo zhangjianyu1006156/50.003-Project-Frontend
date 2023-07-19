09fca3d0334c630325e49b659847895edadcd1cd
applebees2020 <j.applebees2020@gmail.com>
2023-07-19 18:46:30 +0800

    ready for merge

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,11 +1,33 @@
-import React from "react";
-import ResultsRow from "../components/ResultsRow";
+import React, { useState, useEffect } from "react";
+import ResultsList from "../components/ResultsList"
 
 export default function Results(){
+
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        // async
+        // api here
+        const resultsTest = [
+            {
+                id: 1,
+                placename: "Fuji-Q Highland",
+                bookingprice: "60.00",
+                sourcewebsite: "Klook",
+            },
+            {
+                id: 2,
+                placename: "Fuji-Q Highland",
+                bookingprice: "55.00",
+                sourcewebsite: "Trip.com",
+            },
+        ];
+        setResults(resultsTest);
+      }, []);
+
     return(
         <div className="container main content">
-            <ResultsRow />
-            <ResultsRow />
+            <ResultsList results={results}/>
         </div>
-    )
+    );
 }
