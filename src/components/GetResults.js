a348b3a12e1469675e0fd6f7f8bf63ef0385dcce
applebees2020 <j.applebees2020@gmail.com>
2023-07-31 17:56:05 +0800

    this commit is not working yet - added navbar - integrating backend

--- src/components/GetResults.js
+++ src/components/GetResults.js
@@ -1,27 +1,64 @@
-const GetResults = () => {
+// const GetResults = () => {
     
-    const results = [
-        {
-          id: 1,
-          placename: "Fuji-Q Highland",
-          bookingprice: "60.00",
-          sourcewebsite: "Klook",
-          images: ["https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"],
-          rating: 4.5,
-          bookingmode: "Pay Online"
-        },
-        {
-          id: 2,
-          placename: "Fuji-Q Highland",
-          bookingprice: "55.00",
-          sourcewebsite: "Trip.com",
-          images: ["https://images.unsplash.com/photo-1678033595277-c64ac5565e5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80","https://images.unsplash.com/photo-1636010732804-56457b880ba1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"],
-          rating: 3.8,
-          bookingmode: "Pay on Arrival"
-        },
-      ];
+//     const results = [
+//         {
+//           id: 1,
+//           placename: "Fuji-Q Highland",
+//           bookingprice: "60.00",
+//           sourcewebsite: "Klook",
+//           images: ["https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"],
+//           rating: 4.5,
+//           bookingmode: "Pay Online"
+//         },
+//         {
+//           id: 2,
+//           placename: "Fuji-Q Highland",
+//           bookingprice: "55.00",
+//           sourcewebsite: "Trip.com",
+//           images: ["https://images.unsplash.com/photo-1678033595277-c64ac5565e5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80","https://images.unsplash.com/photo-1636010732804-56457b880ba1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"],
+//           rating: 3.8,
+//           bookingmode: "Pay on Arrival"
+//         },
+//       ];
 
-    return results;
+//       // unit test of 
+//       console.log(Array.isArray(results));
+
+//     return results;
+// };
+
+// export default GetResults;
+
+import axios from "axios";
+
+async function GetResults() {
+  try{
+    const response = await axios.get('/products');
+    console.log(Array.isArray(response.data));
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return ([
+              {
+                id: 1,
+                placename: "Fuji-Q Highland",
+                bookingprice: "60.00",
+                sourcewebsite: "Klook",
+                images: ["https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"],
+                rating: 4.5,
+                bookingmode: "Pay Online"
+              },
+              {
+                id: 2,
+                placename: "Fuji-Q Highland",
+                bookingprice: "55.00",
+                sourcewebsite: "Trip.com",
+                images: ["https://images.unsplash.com/photo-1678033595277-c64ac5565e5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80","https://images.unsplash.com/photo-1636010732804-56457b880ba1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"],
+                rating: 3.8,
+                bookingmode: "Pay on Arrival"
+              },
+            ]);
+  }
 };
 
 export default GetResults;
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -3,6 +3,7 @@ import { useAuth } from "../AuthContext/useAuth";
 import ResultsList from "../components/ResultsList";
 import { useNavigate } from "react-router-dom";
 import GetResults from "../components/GetResults";
+import { NavbarSimple } from "../components/Navbar";
 
 export const Results = () => {
   const auth = useAuth();
@@ -23,6 +24,7 @@ export const Results = () => {
 
   return (
     <div>
+      <NavbarSimple/>
       <h1 className="font-bold text-2xl">You reached results!</h1>
       <button onClick={handleLogout}>Log out</button>
       <ResultsList results={results} handleClick={handleClick}/>
