43cac27748bd801b314c60c1a539ee26b57d6b74
rynertan <rynertan@gmail.com>
2023-08-16 06:33:31 +0000

    resolve merge conflict

--- src/components/GetResults.js
+++ src/components/GetResults.js
@@ -20,29 +20,20 @@ const GetResults = async (setResults, filterSource, filter) => {
     console.log(results);
   } catch (error) {
     console.error("Error fetching products:", error);
-    console.error("Error fetching products:", error);
     let resultsPlaceholder = [
       {
         id: 1,
-        placename: "Nankai Rapi:t Kansai Airport Express Ticket",
-        bookingprice: "10.30",
         placename: "Nankai Rapi:t Kansai Airport Express Ticket",
         bookingprice: "10.30",
         sourcewebsite: "Klook",
         images: [
           "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
         ],
-        images: [
-          "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-        ],
         rating: 4.5,
-        bookingmode: "Pay Online",
         bookingmode: "Pay Online",
       },
       {
         id: 2,
-        placename: "FujiQ Highland Ticket",
-        bookingprice: "49.19",
         placename: "FujiQ Highland Ticket",
         bookingprice: "49.19",
         sourcewebsite: "Trip.com",
@@ -50,23 +41,9 @@ const GetResults = async (setResults, filterSource, filter) => {
           "https://images.unsplash.com/photo-1678033595277-c64ac5565e5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80",
           "https://images.unsplash.com/photo-1636010732804-56457b880ba1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
         ],
-        images: [
-          "https://images.unsplash.com/photo-1678033595277-c64ac5565e5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80",
-          "https://images.unsplash.com/photo-1636010732804-56457b880ba1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-        ],
         rating: 3.8,
         bookingmode: "Pay on Arrival",
-        bookingmode: "Pay on Arrival",
       },
-    ];
-    res = resultsPlaceholder;
-    // if (filterSource !== null){
-    //   resultsPlaceholder = resultsPlaceholder.filter(item => item.id !== filterSource.id);
-    // }
-    // setResults(resultsPlaceholder);
-  }
-  if (filterSource !== null) {
-    res = res.filter((item) => item.id !== filterSource.id);
     ];
     res = resultsPlaceholder;
     // if (filterSource !== null){
@@ -85,12 +62,6 @@ const GetResults = async (setResults, filterSource, filter) => {
       }
       if (
         item.name.toLowerCase().includes(filter.dest.toLowerCase()) ||
-        parseFloat(lowerprice) <= parseFloat(filter.budget)
-      ) {
-        return item;
-      }
-      if (
-        item.name.toLowerCase().includes(filter.dest.toLowerCase()) &&
         parseFloat(lowerprice) <= parseFloat(filter.budget)
       ) {
         return item;
